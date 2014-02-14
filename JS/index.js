0dfe2c5e219917f73d9c1ab4934cde33ed1da4cd
kotireddyk <kotireddyk@yahoo.com>
2014-02-14 20:36:54 +0530

    js commit

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
 
-  $("#header3 li").on("click",function(){
-		$(this).css( "background","#ffffff");
-		$(this).animate({"top":"-11px"});
-		
-
+  $("#header3 li").on("mouseenter",function(){
+		$(this).css( {"background":"#ffffff", "top" : "-11px"});
+	});
+  $("#header3 li").on("mouseleave",function(){
+		$(this).css( {"background" : "-webkit-linear-gradient(#ffffff, #eaeaea)" ,
+    "background" : "-o-linear-gradient(#ffffff, #eaeaea)",
+    "background" : "-moz-linear-gradient(#ffffff, #eaeaea)",
+    "background" : "linear-gradient(#ffffff, #eaeaea)"});
 	});
 
 });
